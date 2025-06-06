0020c45a5a525fdf2390d8c79c0b03774f4b61bc
JorgeV <vilcajorge0327@gmail.com>
2025-06-06 04:28:46 -0300

    3ra edicion
    ya esta casi listo, toda la estrcutura y funcion, solo falta agregar imagenes y caracteristicas

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-function cambiarColor() {
-  const colores = ["#f4f4f4", "#d0e8f2", "#ffe8cc", "#e0ffe0", "#fff0f5"];
-  const colorActual = document.body.style.backgroundColor;
-  let nuevoColor;
-
-  do {
-    nuevoColor = colores[Math.floor(Math.random() * colores.length)];
-  } while (nuevoColor === colorActual);
-
-  document.body.style.backgroundColor = nuevoColor;
+function mostrarSeccion(seccionId){
+  const secciones= document.querySelectorAll('.productos');
+  secciones.forEach(sec => {
+    sec.style.display= 'none';
+  });
+  const seleccionada = document.getElementById(seccionId);
+  if(seleccionada){
+    seleccionada.style.display='block';
+  }
 }
